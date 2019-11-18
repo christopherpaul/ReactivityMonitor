76b392b05c74b43de0908c29c164362046ea43c2
Chris White <christopherpaul@users.noreply.github.com>
2019-11-18 16:22:37 +0000

    Rejig enumerator class to use a pImpl.

--- ReactivityProfiler/ProfilerInfo.h
+++ ReactivityProfiler/ProfilerInfo.h
@@ -6,107 +6,122 @@ template<typename Token, size_t batchSize = 10>
 class CCorEnum
 {
 public:
-    typedef std::function<HRESULT(IMetaDataImport2 *, HCORENUM*, Token*, ULONG, ULONG*)> enumFunction_t;
+    typedef std::function<HRESULT(IMetaDataImport2*, HCORENUM*, Token*, ULONG, ULONG*)> enumFunction_t;
 
-    CCorEnum(IMetaDataImport2* metadata, const enumFunction_t& enumFunction) :
-        m_metadata(metadata),
-        m_enumFunction(enumFunction),
-        m_enumHandle(0),
-        m_vector(batchSize),
-        m_countInVector(0),
-        m_current(0)
+private:
+    class Impl
     {
-    }
-
-    ~CCorEnum()
-    {
-        if (m_enumHandle)
+    public:
+        Impl(IMetaDataImport2* metadata, const enumFunction_t& enumFunction) :
+            m_metadata(metadata),
+            m_enumFunction(enumFunction),
+            m_enumHandle(0),
+            m_vector(batchSize),
+            m_countInVector(0),
+            m_current(0)
         {
-            m_metadata->CloseEnum(m_enumHandle);
         }
+
+        Impl(const Impl&) = delete;
+        Impl& operator=(const Impl&) = delete;
+
+        ~Impl()
+        {
+            if (m_enumHandle)
+            {
+                m_metadata->CloseEnum(m_enumHandle);
+            }
+        }
+
+        size_t Count()
+        {
+            if (!m_enumHandle)
+            {
+                FetchBatch();
+            }
+
+            ULONG count;
+            CHECK_SUCCESS(m_metadata->CountEnum(m_enumHandle.get(), &count));
+            return count;
+        }
+
+        bool MoveNext()
+        {
+            if (!m_enumHandle)
+            {
+                FetchBatch();
+            }
+            else
+            {
+                m_current++;
+            }
+
+            if (m_current >= m_countInVector)
+            {
+                if (m_countInVector < batchSize)
+                {
+                    return false;
+                }
+
+                FetchBatch();
+            }
+
+            return m_current < m_countInVector;
+        }
+
+        Token Current() const
+        {
+            if (!m_enumHandle)
+            {
+                RELTRACE("Current called before MoveNext");
+                throw E_FAIL;
+            }
+            else if (m_current >= m_countInVector)
+            {
+                RELTRACE("Current called after end of enumeration");
+                throw E_FAIL;
+            }
+
+            return m_vector[m_current];
+        }
+
+    private:
+        IMetaDataImport2* const m_metadata;
+        const enumFunction_t m_enumFunction;
+        HCORENUM m_enumHandle;
+        std::vector<Token> m_vector;
+        int m_current;
+        size_t m_countInVector;
+
+        void FetchBatch()
+        {
+            ULONG count;
+            CHECK_SUCCESS(m_enumFunction(m_metadata, &m_enumHandle, m_vector.data(), batchSize, &count));
+            m_countInVector = count;
+            m_current = 0;
+        }
+    };
+
+public:
+    CCorEnum(IMetaDataImport2* metadata, const enumFunction_t& enumFunction) :
+        m_pImpl(new Impl(metadata, enumFunction))
+    {
     }
 
     CCorEnum(const CCorEnum& other) = delete;
     CCorEnum& operator=(const CCorEnum& other) = delete;
 
-    CCorEnum(CCorEnum&& other) noexcept :
-        m_metadata(other.m_metadata),
-        m_enumFunction(other.m_enumFunction),
-        m_enumHandle(0),
-        m_vector(std::move(other.m_vector)),
-        m_countInVector(other.m_countInVector),
-        m_current(other.m_current)
+    CCorEnum(CCorEnum&& other) noexcept
     {
-        std::swap(m_enumHandle, other.m_enumHandle);
+        std::swap(m_pImpl, other.m_pImpl);
     }
 
-    size_t Count()
-    {
-        if (!m_enumHandle)
-        {
-            FetchBatch();
-        }
-
-        ULONG count;
-        CHECK_SUCCESS(m_metadata->CountEnum(m_enumHandle.get(), &count));
-        return count;
-    }
-
-    bool MoveNext()
-    {
-        if (!m_enumHandle)
-        {
-            FetchBatch();
-        }
-        else
-        {
-            m_current++;
-        }
-
-        if (m_current >= m_countInVector)
-        {
-            if (m_countInVector < batchSize)
-            {
-                return false;
-            }
-
-            FetchBatch();
-        }
-
-        return m_current < m_countInVector;
-    }
-
-    Token Current() const
-    {
-        if (!m_enumHandle)
-        {
-            RELTRACE("Current called before MoveNext");
-            throw E_FAIL;
-        }
-        else if (m_current >= m_countInVector)
-        {
-            RELTRACE("Current called after end of enumeration");
-            throw E_FAIL;
-        }
-
-        return m_vector[m_current];
-    }
+    size_t Count() { return m_pImpl->Count(); }
+    bool MoveNext() { return m_pImpl->MoveNext(); }
+    Token Current() { return m_pImpl->Current(); }
 
 private:
-    IMetaDataImport2* const m_metadata;
-    const enumFunction_t m_enumFunction;
-    HCORENUM m_enumHandle;
-    std::vector<Token> m_vector;
-    int m_current;
-    size_t m_countInVector;
-
-    void FetchBatch()
-    {
-        ULONG count;
-        CHECK_SUCCESS(m_enumFunction(m_metadata, &m_enumHandle, m_vector.data(), batchSize, &count));
-        m_countInVector = count;
-        m_current = 0;
-    }
+    std::unique_ptr<Impl> m_pImpl;
 };
 
 struct ModuleInfo
