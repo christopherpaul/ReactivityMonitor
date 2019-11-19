21c2837a18ec6908132e754f451d086a55e5d36a
Chris White <christopherpaul@users.noreply.github.com>
2019-11-19 14:53:18 +0000

    Add a support assembly project.
    Doesn't do much and isn't used yet!
    Update build to get the assembly copied alongside the profiler DLL.

---
+++ ReactivityProfiler.Support/Instrument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactivityProfiler.Support
+{
+    public static class Instrument
+    {
+        public static IObservable<T> Returned<T>(IObservable<T> observable, int instrumentationPoint)
+        {
+            return observable;
+        }
+    }
+}
