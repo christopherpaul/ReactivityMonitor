4027e63c28699835ad62588cb50014635a4e4376
Chris White <christopherpaul@users.noreply.github.com>
2019-11-20 10:53:14 +0000

    Add the instrumentation classes from OpenProfiler.
    Also, remove AnyCPU platform from solution.

---
+++ ReactivityProfiler/Instrumentation/ExceptionHandler.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "ExceptionHandler.h"
+
+namespace Instrumentation
+{
+	ExceptionHandler::ExceptionHandler()
+	{
+		m_tryStart = nullptr;
+		m_tryEnd = nullptr;
+		m_handlerStart = nullptr;
+		m_handlerEnd = nullptr;
+		m_filterStart = nullptr;
+		m_token = 0;
+		m_handlerType = COR_ILEXCEPTION_CLAUSE_NONE;
+	}
+}
