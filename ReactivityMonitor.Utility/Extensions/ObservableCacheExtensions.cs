6284fd6c70e684917f049b695a420deac1c26e57
Chris White <christopherpaul@users.noreply.github.com>
2019-12-28 11:33:35 +0000

    Fix SemiJoinOnRightKey so it produces sensible change sets.
    Initial implementation re-added whole group (with same right key) every time another member was
    added to it.

--- ReactivityMonitor.Utility/Extensions/ObservableCacheExtensions.cs
+++ ReactivityMonitor.Utility/Extensions/ObservableCacheExtensions.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Linq;
 using System.Text;
+using System.Reactive.Subjects;
+using System.Reactive;
 
 namespace ReactivityMonitor.Utility.Extensions
 {
@@ -12,9 +14,27 @@ namespace ReactivityMonitor.Utility.Extensions
         public static IObservable<IChangeSet<TLeft, TLeftKey>> SemiJoinOnRightKey<TLeft, TLeftKey, TRight, TRightKey>(
             this IObservable<IChangeSet<TLeft, TLeftKey>> left, IObservable<IChangeSet<TRight, TRightKey>> right, Func<TLeft, TRightKey> leftItemKeySelector)
         {
-            return right.InnerJoinMany(left, leftItemKeySelector, (rightItem, leftGroup) => leftGroup)
-                .TransformMany(leftGroup => leftGroup.KeyValues, kvp => kvp.Key)
-                .Transform(kvp => kvp.Value);
+            var rightKeys = right.Transform((_, k) => k);
+
+            return left.Group(leftItemKeySelector)
+                .InnerJoin(rightKeys, k => k, (l, r) => l)
+                .Transform(group =>
+                {
+                    var whenGroupRemoved = new Subject<Unit>();
+
+                    var removeGroupItemsChangeSet = Observable.Defer(() =>
+                        Observable.Return(new ChangeSet<TLeft, TLeftKey>(
+                            group.Cache.KeyValues.Select(kvp => new Change<TLeft, TLeftKey>(ChangeReason.Remove, kvp.Key, kvp.Value)))));
+
+                    var cacheWithRemovals =
+                        group.Cache.Connect()
+                            .TakeUntil(whenGroupRemoved)
+                            .Concat(removeGroupItemsChangeSet);
+
+                    return new { cacheWithRemovals, whenGroupRemoved };
+                })
+                .OnItemRemoved(g => g.whenGroupRemoved.OnNext(default))
+                .MergeMany(g => g.cacheWithRemovals);
         }
     }
 }
