4deef2acf47ebb444eedc3bbaf605c1dac1949e2
Chris White <christopherpaul@users.noreply.github.com>
2020-01-18 12:20:33 +0000

    Switch to specifying profiler by path
    When launching program to profile, set COR_PROFILER_PATH_[32|64] and use the CLSID, rather than
    relying on registry lookup.
    
    Disable the automatic registration of the profiler when it's built (mainly so I can be sure it's
    are working via the path mechanism).
    
    Specifying Prefer32Bit=true parameter to TestProfilee now puts output in a different folder to
    avoid confusion.

--- ReactivityMonitor/Services/ConnectionService.cs
+++ ReactivityMonitor/Services/ConnectionService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,6 +18,8 @@ namespace ReactivityMonitor.Services
     {
         private readonly ServerDiscovery mServerDiscovery;
         private readonly ISubject<IConnectionModel> mConnectionSubject;
+        private readonly string mProfilerLocation32;
+        private readonly string mProfilerLocation64;
 
         public ConnectionService()
         {
@@ -30,6 +34,16 @@ namespace ReactivityMonitor.Services
             }, server => server.ProcessId);
 
             mConnectionSubject = new BehaviorSubject<IConnectionModel>(null);
+
+            var profilersLocation =
+                Path.Combine(
+                    Path.GetDirectoryName(
+                        new Uri(Assembly.GetEntryAssembly().CodeBase).LocalPath),
+                    "profiler");
+
+            const string cProfilerDll = "ReactivityProfiler.dll";
+            mProfilerLocation32 = Path.Combine(profilersLocation, "Win32", cProfilerDll);
+            mProfilerLocation64 = Path.Combine(profilersLocation, "x64", cProfilerDll);
         }
 
         public IObservable<IChangeSet<Server, int>> AvailableServers { get; }
@@ -44,7 +58,9 @@ namespace ReactivityMonitor.Services
 
             psi.UseShellExecute = false;
             psi.Environment.Add("COR_ENABLE_PROFILING", "1");
-            psi.Environment.Add("COR_PROFILER", "ReactivityMonitor.RxProfiler.1");
+            psi.Environment.Add("COR_PROFILER_PATH_32", mProfilerLocation32);
+            psi.Environment.Add("COR_PROFILER_PATH_64", mProfilerLocation64);
+            psi.Environment.Add("COR_PROFILER", "{09c5b5d7-62d2-4448-911d-2e1346a21110}");
 
             Process process;
             try
