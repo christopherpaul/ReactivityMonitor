d36214a48eec9ae30e90cfce869e4322e0f800c4
Chris White <christopherpaul@users.noreply.github.com>
2020-03-15 16:10:22 +0000

    Mini-refactor of Selection
    Make it less painful to add more kinds of item to it

--- ReactivityMonitor/Services/Selection.cs
+++ ReactivityMonitor/Services/Selection.cs
@@ -12,16 +12,16 @@ namespace ReactivityMonitor.Services
 {
     public sealed class Selection
     {
-        public static Selection Empty { get; } = new Selection(null, null, ImmutableList<EventItem>.Empty, null, ImmutableList<IInstrumentedCall>.Empty);
+        public static Selection Empty { get; } = new Selection(default, default, default);
 
-        private Selection(IWorkspace workspace, EventItem primaryEventItem, IImmutableList<EventItem> selectedEventItems,
-            IInstrumentedCall primaryInstrumentedCall, IImmutableList<IInstrumentedCall> selectedInstrumentedCalls)
+        private readonly Selected<IInstrumentedCall> mCallSelection;
+        private readonly Selected<EventItem> mEventSelection;
+
+        private Selection(IWorkspace workspace, Selected<IInstrumentedCall> callSelection, Selected<EventItem> eventSelection)
         {
             Workspace = workspace;
-            PrimaryEventItem = primaryEventItem;
-            SelectedEventItems = selectedEventItems;
-            PrimaryInstrumentedCall = primaryInstrumentedCall;
-            SelectedInstrumentedCalls = selectedInstrumentedCalls;
+            mCallSelection = callSelection;
+            mEventSelection = eventSelection;
         }
 
         public Selection SetWorkspace(IWorkspace workspace)
@@ -31,64 +31,82 @@ namespace ReactivityMonitor.Services
                 return this;
             }
 
-            return new Selection(workspace, null, Empty.SelectedEventItems, null, Empty.SelectedInstrumentedCalls);
+            return new Selection(workspace, mCallSelection, mEventSelection);
         }
 
         public Selection SetEvent(EventItem eventItem)
         {
-            return ChangeEvents(eventItem, ImmutableList.Create(eventItem));
+            return ChangeEvents(mEventSelection.Single(eventItem));
         }
 
         public Selection AddEvent(EventItem eventItem)
         {
-            return ChangeEvents(eventItem, SelectedEventItems.Add(eventItem));
+            return ChangeEvents(mEventSelection.Add(eventItem));
         }
 
         public Selection RemoveEvent(EventItem eventItem)
         {
-            return ChangeEvents(eventItem == PrimaryEventItem ? null : PrimaryEventItem, SelectedEventItems.Remove(eventItem));
+            return ChangeEvents(mEventSelection.Remove(eventItem));
         }
 
         public Selection ClearEvent()
         {
-            return ChangeEvents(null, Empty.SelectedEventItems);
+            return ChangeEvents(default);
         }
 
-        private Selection ChangeEvents(EventItem primary, IImmutableList<EventItem> selected)
+        private Selection ChangeEvents(Selected<EventItem> eventSelection)
         {
-            return new Selection(Workspace, primary, selected, PrimaryInstrumentedCall, SelectedInstrumentedCalls);
+            return new Selection(Workspace, mCallSelection, eventSelection);
         }
 
         public Selection SetCall(IInstrumentedCall call)
         {
-            return ChangeCalls(call, ImmutableList.Create(call));
+            return ChangeCalls(mCallSelection.Single(call));
         }
 
         public Selection AddCall(IInstrumentedCall call)
         {
-            return ChangeCalls(call, SelectedInstrumentedCalls.Add(call));
+            return ChangeCalls(mCallSelection.Add(call));
         }
 
         public Selection RemoveCall(IInstrumentedCall call)
         {
-            return ChangeCalls(call == PrimaryInstrumentedCall ? null : PrimaryInstrumentedCall,
-                SelectedInstrumentedCalls.Remove(call));
+            return ChangeCalls(mCallSelection.Remove(call));
         }
 
         public Selection ClearCall()
         {
-            return ChangeCalls(null, Empty.SelectedInstrumentedCalls);
+            return ChangeCalls(default);
         }
 
-        private Selection ChangeCalls(IInstrumentedCall primary, IImmutableList<IInstrumentedCall> selected)
+        private Selection ChangeCalls(Selected<IInstrumentedCall> callSelection)
         {
-            return new Selection(Workspace, PrimaryEventItem, SelectedEventItems, primary, selected);
+            return new Selection(Workspace, callSelection, mEventSelection);
         }
 
         public IWorkspace Workspace { get; }
-        public EventItem PrimaryEventItem { get; }
-        public IImmutableList<EventItem> SelectedEventItems { get; }
-        public IInstrumentedCall PrimaryInstrumentedCall { get; }
-        public IImmutableList<IInstrumentedCall> SelectedInstrumentedCalls { get; }
+        public EventItem PrimaryEventItem => mEventSelection.Primary;
+        public IImmutableList<EventItem> SelectedEventItems => mEventSelection.All;
+        public IInstrumentedCall PrimaryInstrumentedCall => mCallSelection.Primary;
+        public IImmutableList<IInstrumentedCall> SelectedInstrumentedCalls => mCallSelection.All;
+
+        private struct Selected<T> where T : class
+        {
+            private readonly IImmutableList<T> mAll;
+
+            private Selected(T primary, IImmutableList<T> all)
+            {
+                Primary = primary;
+                mAll = all;
+            }
+
+            public T Primary { get; }
+            public IImmutableList<T> All => mAll ?? ImmutableList<T>.Empty;
+
+            public Selected<T> None => default;
+            public Selected<T> Single(T item) => new Selected<T>(item, ImmutableList.Create(item));
+            public Selected<T> Add(T item) => item == null ? this : new Selected<T>(item, All.Add(item));
+            public Selected<T> Remove(T item) => item == null ? this : new Selected<T>(item == Primary ? null : Primary, All.Remove(item));
+        }
     }
 }
